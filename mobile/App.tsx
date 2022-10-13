ce76675987d8eefa9107a9dafbedbfe2d6a8b563
Dafne Ribeiro <106087250+DafneRibeiro@users.noreply.github.com>
2022-10-13 18:10:14 +0100

    mobile first part

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -5,10 +5,23 @@ import {
   Inter_600SemiBold,
   Inter_700Bold,
   Inter_900Black
-} from '@expo-google-fonts/inter'
+} from '@expo-google-fonts/inter';
+
 import { Background } from './src/components/Background';
+import { Home } from './src/screens/Home';
+import { Loading } from './src/components/Loading';
+
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  });
+
+
   return (
     <Background>
       <StatusBar
@@ -17,6 +30,7 @@ export default function App() {
         translucent
 
       />
+      {fontsLoaded ? <Home /> : <Loading />}
     </Background>
   );
 }
