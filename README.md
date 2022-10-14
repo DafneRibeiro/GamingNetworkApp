ebde560ffc180d9ccb088fcc8acab484faafaeed
Dafne Ribeiro <106087250+DafneRibeiro@users.noreply.github.com>
2022-10-14 22:23:07 +0100

    Backend set up and models created

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # GamingNetworkApp
-NLW eSports
+NLW eSports By RocketSeat 
+
+## Goals of the project
+---
+
+* Practice ReactJS
+* Learn more about TypeScrypt
+* Learn Tailwind
+* Create a frontend that works and is responsive
+* Have my first contact with ReactNative
+* To learn and develop my backend skills and my web design skills
+
+
+This project is a Lab project developed by RocketSeat with the purpose of studying and practicing your skills with the tutors.
+
+
+
+## Tech Stack 
+
+* TypeScrypt with Tailwind
+* NodeJs
+* ExpressJs
+* ReactNative
--- server/src/server.ts
+++ server/src/server.ts
@@ -5,13 +5,37 @@ const app = express()
 //first parameter will the address the user will be accessing for example: www.uol.com/brasil (i need to use everything after / including it)
 
 // and the second is what function will be execute when the user acess the route /ads 
-app.get('/ads', (request, response) => {
+app.get('/games', (request, response) => {
+  return response.json([]);
+})
+
+app.post('/ads', (request, response) => {
+  return response.status(201).json([]);
+})
+
+
+app.get('games/:id/ads', (request, response) => { //id is a params so i need ':'
   //req => search for info inside the request and res => returns the request
+
+  //const gameId = request.params.id;
+
+  //return response.send(gameId);
+
+
   return response.json([
-    { id: 1, name: 'Anuncio 1' },
-    { id: 2, name: 'Anuncio 2' },
-    { id: 3, name: 'Anuncio 3' },
+    { id: 1, name: 'Ad 1' },
+    { id: 2, name: 'Ad 2' },
+    { id: 3, name: 'Ad 3' },
+    { id: 4, name: 'Ad 4' },
+    { id: 5, name: 'Ad 5' },
   ])
 })
 
+app.get('ads/:id/ads', (request, response) => {
+  //const adId = request.params.id;
+
+  return response.json([
+
+  ])
+})
 app.listen(3333)
